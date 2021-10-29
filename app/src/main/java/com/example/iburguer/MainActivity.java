2f81640705c04b14a18af7794b1ade9e9061a93c
Thiago Piffer <thiago.piffer@hotmail.com>
2021-10-28 21:46:00 -0300

    Melhorias nos códigos; Envio de dados via Bundle

--- app/src/main/java/com/example/iburguer/MainActivity.java
+++ app/src/main/java/com/example/iburguer/MainActivity.java
@@ -5,24 +5,59 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.iburguer.utils.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout frameLayout;
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final FirebaseUser user = mAuth.getCurrentUser();
+    DatabaseReference reference;
+
+    String nomeCliente;
+    Bundle clienteData = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         BottomNavigationView menu_navegacao = findViewById(R.id.menu_navegacao);
-        menu_navegacao.setOnNavigationItemSelectedListener(bottomNavMethod);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new PrincipalFragment()).commit();
+        reference = FirebaseDatabase.getInstance().getReference("clientes");
+
+        reference.child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    if(task.getResult().exists()) {
+                        DataSnapshot dataSnapshot = task.getResult();
+
+                        nomeCliente = String.valueOf(dataSnapshot.child(Constants.NOME_CLIENTE).getValue());
+                        clienteData.putString(Constants.NOME_CLIENTE, nomeCliente);
+
+                        menu_navegacao.setOnNavigationItemSelectedListener(bottomNavMethod);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new PrincipalFragment().newInstance(clienteData)).commit();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Falha na leitura de dados: usuário não existe!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Falha ao ler dados do banco!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -31,13 +66,13 @@ public class MainActivity extends AppCompatActivity {
             Fragment frag = null;
             switch (item.getItemId()){
                 case R.id.page_home:
-                    frag = new PrincipalFragment();
+                    frag = new PrincipalFragment().newInstance(clienteData);
                 break;
                 case R.id.page_pedidos:
-                    frag = new PedidosFragment();
+                    frag = new PedidosFragment().newInstance(clienteData);
                     break;
                 case R.id.page_conta:
-                    frag = new ContaFragment();
+                    frag = new ContaFragment().newInstance(clienteData);
                     break;
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.container, frag).commit();
--- app/src/main/java/com/example/iburguer/PrincipalFragment.java
+++ app/src/main/java/com/example/iburguer/PrincipalFragment.java
@@ -2,43 +2,35 @@ package com.example.iburguer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.iburguer.utils.Constants;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class PrincipalFragment extends Fragment {
 
-    private Bundle extras;
+    private String nomeCliente;
     private TextView textClienteNome;
-    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private final FirebaseUser user = mAuth.getCurrentUser();
-    DatabaseReference reference;
 
     public PrincipalFragment() {}
 
-    public PrincipalFragment(Bundle extras) {
-        this.extras = extras;
+    public static PrincipalFragment newInstance(Bundle clienteData) {
+        PrincipalFragment fragment = new PrincipalFragment();
+        fragment.setArguments(clienteData);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            nomeCliente = getArguments().getString(Constants.NOME_CLIENTE);
+        }
     }
 
     @Override
@@ -50,43 +42,16 @@ public class PrincipalFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-        Handler handler = new Handler();
-        new Thread() {
-            public void run() {
-                handler.post(new Runnable() {
-                    public void run() {
-                        Button testeLoja = (Button) getActivity().findViewById(R.id.lojateste);
-                        testeLoja.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                startActivity(new Intent(getActivity(), LojaActivity.class));
-                            }
-                        });
-                    }
-                });
-            }
-        }.start();
+        Button testeLoja = (Button) view.findViewById(R.id.lojateste);
         textClienteNome = view.findViewById(R.id.textClienteNome);
-        readDataFromCliente();
-    }
 
-    private void readDataFromCliente() {
-        reference = FirebaseDatabase.getInstance().getReference("clientes");
-        reference.child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        testeLoja.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    if(task.getResult().exists()) {
-                        DataSnapshot dataSnapshot = task.getResult();
-                        textClienteNome.setText(String.valueOf(dataSnapshot.child(Constants.NOME_CLIENTE).getValue()));
-                    } else {
-                        Toast.makeText(getContext(), "Falha na leitura de dados: usuário não existe!", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(getContext(), "Falha ao ler dados do banco!", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), LojaActivity.class));
             }
         });
+        textClienteNome.setText(nomeCliente);
     }
 
 }
